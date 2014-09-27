7dac9a05239683f7f852ee4057a90f2e5023e691
brint <brint@blendimc.com>
2014-09-27 13:31:33 -0500

    Icomoon Updates

--- microfaction/controller/user-panel/index.php
+++ microfaction/controller/user-panel/index.php
@@ -1,7 +1,7 @@
 <?php if(!defined("CONF_PATH")) { die("No direct script access allowed."); }
 
 // Load Forum-Specific Panel Links
-$userPanel['Subscriptions']['Subscriptions'] = "/user-panel/subscriptions";
+$userPanel['Subscriptions']['Subscriptions'] = "/subscriptions";
 
 // Reorder the Panel
 $userPanel = array('Subscriptions' => $userPanel['Subscriptions']) + $userPanel;
